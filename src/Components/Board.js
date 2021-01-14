94afdefaaf24b8be128e4ac0065d529266b7174b
Colman Currie <curriecolman@gmail.com>
2021-01-14 11:48:46 -0800

    add onClick to Board component

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -4,15 +4,18 @@ import '../../src/App.css';
 
 class Board extends Component {
     renderSquare(i) {
-      return <Square value={i} />;
+      return (  
+        <Square 
+            value={this.props.squares[i]}
+            onClick={() => this.props.onClick(i)} 
+        />
+        );
     }
+
   
     render() {
-      const status = 'Next player: X';
-  
       return (
         <div>
-          <div className="status">{status}</div>
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}

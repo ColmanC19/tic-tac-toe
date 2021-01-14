712b60422e1556dbe837e0401b022776da6b8c18
Colman Currie <curriecolman@gmail.com>
2021-01-14 11:04:23 -0800

    turn Square from class based to common func

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,20 +1,12 @@
 import React, { Component } from 'react';
 import '../../src/App.css';
 
-class Square extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        }
-    }
-    render() {
-        return (
-            <button className="square" onCLick={() => this.setState({value: 'X'})}>
-                {this.props.value}
-            </button>
-        )
-    }
-};
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
 
 export default Square;
